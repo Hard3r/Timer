f02935e134eecfd1ab5ac12a2118549b36d9aeb0
Zeus <Hard3r>
2016-03-08 21:17:07 +0300

    scroll camera

--- GameScreen.swift
+++ GameScreen.swift
@@ -13,6 +13,10 @@ class GameScreen: SKScene {
     
     var label: SKLabelNode!
     var selectedheroes: Set<String>!
+    var cam: SKCameraNode!
+    var test:CGFloat = 0;
+    var delta:CGFloat? = 0;
+    var olddelta:CGFloat? = 0;
     
     
     override init() {
@@ -31,6 +35,9 @@ class GameScreen: SKScene {
     }
    
     override func didMoveToView(view: SKView) {
+        
+        self.anchorPoint = CGPointMake(0, 0)
+        
         var test: String = ""
         for lel in selectedheroes {
             test = test + " \n" + lel
@@ -38,23 +45,40 @@ class GameScreen: SKScene {
         }
         
         
+        let background2 = SKSpriteNode(imageNamed: "dota2walpapaer");
+        background2.position = CGPointMake(0, 0)
+        background2.zPosition = 0;
+        background2.anchorPoint = CGPointMake(0, 0)
+        background2.setScale(0.8)
+        self.addChild(background2);
         
-        
-        
-        
-        var Second = SKLabelNode(text: test)
-        Second.fontName = "Chalkboard SE Bold"  // задаем имя шрифта.
-        Second.fontColor = SKColor.whiteColor() // задаем цвет шрифта.
-        Second.position = CGPointMake(frame.midX, frame.midY) // задаем позицию.
-        Second.fontSize = 20 // задаем размер шрифта.
-        Second.name = "Second" // задаем имя спрайта
-        self.addChild(Second) // добавляем наш спрайт на нашу сцену.
-        
+        cam = SKCameraNode();
+        self.camera = cam
+        cam.position = CGPointMake(self.frame.midX, self.frame.midY)
         
         
     }
     
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch: AnyObject in touches {
+            let start = touch.locationInNode(self)
+            let previousLocation = touch.previousLocationInNode(self)
+            //let deltaY = start.y - previousLocation.y
+            delta = start.y - previousLocation.y
+        }
+    } 
+    
     override func update(currentTime: NSTimeInterval) {
-        //label.text = test
+        
+        while (olddelta != delta) {
+            if cam.position.y > 500 {
+                cam.position.y = 500
+            } else if cam.position.y < -100 {
+                cam.position.y = -100
+                } else {
+                cam.position.y += -delta! }
+            
+                olddelta = delta
+            }
     }
 }
